2714bf79e1b37c9edc9ca29c672a09afe9c8bee7
Simone Menichetti <simon3menichetti95@gmail.com>
2025-02-05 11:38:20 +0100

    Inizializzazione progetto meteo-app con Angular e Bootstrap, implementazione API meteo e suggerimenti città

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { WeatherComponent } from './weather/weather.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, WeatherComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meteo-app';
+  title = 'IoWeather';
 }
